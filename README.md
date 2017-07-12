79db19ab827aac32c640c61950160f8c1bd524e6
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2017-07-12 18:32:22 -0300

    README feito

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ep3-219
+
+- execute o <Makefile> em cada máquina
+- configure o SSH das máquinas
+- gere um arquivo com nome <hosfile> contendo a lista dos IPs das máquinas que irão receber o código distribuído
+- rode o <run_measurements.sh>.
+- use o <gera_boxplot.py> com o python para gerar os gráficos.
+
--- gera_boxplot.py
+++ gera_boxplot.py
@@ -47,6 +47,7 @@ amostras = {
 	}  
 }
 
+maximo = {'full' : 0, 'seahorse' : 0, 'elephant' : 0, 'triple' : 0}
 for results in lista_results[:4]:
 	for diretorio in lista_diretorios:
 		lista_arquivos = os.listdir(results+"/"+diretorio)
@@ -69,6 +70,8 @@ for results in lista_results[:4]:
 
 			amostras[modo_arq][nome_regiao][diretorio][results] = numeros            
 			amostras[modo_arq][nome_regiao][diretorio][results].sort()
+			if amostras[modo_arq][nome_regiao][diretorio][results][-1] > maximo[nome_regiao]:
+				maximo[nome_regiao] = amostras[modo_arq][nome_regiao][diretorio][results][-1]
 
 			arq.close()
 			
@@ -76,33 +79,58 @@ for results in lista_results[:4]:
 ###########################################
 
 # Cria o boxplot
+
+
 for modoKey, modo in amostras.items():
 	for regiaoKey, regiao in modo.items():
 
 		# Cria uma instancia de figura e eixos
 		fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 9))
 		data_to_plot = []
-
 		for i in range (nCod):
 			tipoCodigo = regiao[lista_diretorios[i]]
+
+			figIndivual, axIndivual = plt.subplots(nrows=1, ncols=1, figsize=(10, 9))			
+			data_to_plotIndivual = []
+			
 			for j in range (nConf):
 				tipoConfig = tipoCodigo[lista_results[j]]
-				data_to_plot.append(tipoConfig)			
+				data_to_plot.append(tipoConfig)
+				data_to_plotIndivual.append(tipoConfig)		
 			data_to_plot.append([0,0,0,0,0,0,0,0,0,0]) #para criar um espaco entre os result dos dif codigos
+			box_plotIndivual = axIndivual.boxplot(data_to_plotIndivual)
+			axIndivual.set_xticklabels(["8maq_1core", "4maq_2core", "2maq_4core", "1maq_8core"], rotation = 90)
+			axIndivual.yaxis.grid(True)
+
+			plt.xlabel('Número de máquinas e cores', fontsize=10, color='red')
+			plt.ylabel('Tempo Em Segundos', fontsize=10, color='red')	
+
+			title = 'regiao: '+regiaoKey+' '+modoKey+' I/O e aloc. memoria com N = 8192 '+lista_diretorios[i]
+			plt.title(title, fontsize=10)
+
+			## Remove marcadores de eixos
+			axIndivual.get_xaxis().tick_bottom()
+			axIndivual.get_yaxis().tick_left()
+
+			maxRegiao = round(maximo[regiaoKey], 5) 
+			interval = round(maxRegiao/30.0, 5)
+			
+			plt.yticks(np.arange(0, maxRegiao + interval, interval))
+
+			# Salva a figura
+			figIndivual.savefig('graphics/'+lista_diretorios[i]+'/'+regiaoKey+'_'+modoKey+'.png', bbox_inches='tight')
+			plt.cla()
+			plt.close()
+
+
+
 		box_plot = ax.boxplot(data_to_plot)
 		
-		maximo = 0
-		
-		for amostra in data_to_plot:
-			if amostra[-1] > maximo:
-				maximo = amostra[-1]
-
-		ax.set_xticklabels(["8maq_1core_seq", "4maq_2core_seq", "2maq_4core_seq", "1maq_8core_seq","","8maq_1core_openMPI_seq", "4maq_2core_openMPI_seq", "2maq_4cor_openMPI_seq", "1maq_8core_seq_openMPI_seq","","8maq_1cor_openMPI_omp", "4maq_2core_openMPI_omp", "2maq_4core_openMPI_omp", "1maq_8core_openMPI_omp"], rotation = 90)
+		ax.set_xticklabels(["8maq_1core_seq", "4maq_2core_seq", "2maq_4core_seq", "1maq_8core_seq","","8maq_1core_openMPI_seq", "4maq_2core_openMPI_seq", "2maq_4cor_openMPI_seq", "1maq_8core_openMPI_seq","","8maq_1cor_openMPI_omp", "4maq_2core_openMPI_omp", "2maq_4core_openMPI_omp", "1maq_8core_openMPI_omp"], rotation = 90)
 		ax.yaxis.grid(True)
 
 		plt.xlabel('Número de máquinas e cores', fontsize=10, color='red')
-		plt.ylabel('Tempo Em Segundos', fontsize=10, color='red')
-		
+		plt.ylabel('Tempo Em Segundos', fontsize=10, color='red')	
 
 		title = 'regiao: '+regiaoKey+' '+modoKey+' I/O e aloc. memoria com N = 8192'
 		plt.title(title, fontsize=10)
@@ -111,13 +139,15 @@ for modoKey, modo in amostras.items():
 		ax.get_xaxis().tick_bottom()
 		ax.get_yaxis().tick_left()
 
-		maximo = round(maximo, 5) 
-		interval = round(maximo/30.0, 5)
-		print "maximo = ", maximo
+		maxRegiao = round(maximo[regiaoKey], 5) 
+		interval = round(maxRegiao/30.0, 5)
+		print "maximo = ", maxRegiao
 		print "interval = ", interval
 		print "*************************"
 
-		plt.yticks(np.arange(0, maximo + interval, interval))
+		plt.yticks(np.arange(0, maxRegiao + interval, interval))
 
 		# Salva a figura
 		fig.savefig('graphics/'+regiaoKey+'_'+modoKey+'.png', bbox_inches='tight')
+		plt.cla()
+		plt.close()
